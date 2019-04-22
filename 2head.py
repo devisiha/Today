9bf300a0052952c87527538a127f9594b1c3cfd8
Teacher <devisiha@gmail.com>
2019-04-22 13:09:24 +0530

    modified head()

--- 2head.py
+++ 2head.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df=pd.DataFrame({'ID':[1,2,3],'Mark1':[30,50,90],'Mark2':[80,50,40]})
+df=pd.DataFrame({'ID':[1,2,3,4],'Mark1':[30,50,90,60],'Mark2':[80,50,40,50]})
 print(df.head(2))
